024e29a18292f74e3dc2e7ce543d6a055eff2527
JM <pok_nima@naver.com>
2025-03-06 10:03:39 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        String str = String.valueOf(n);
+        for (int i = 0; i < str.length(); i ++) {
+            answer += Integer.valueOf(str.substring(i, i+1));
+        }
+        return answer;
+    }
+}
